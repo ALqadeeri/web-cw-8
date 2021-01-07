ac6668dccc1a1c5907df3147af4582468daea040
ALqadeeri <w.alqadeeri@gmail.com>
2021-01-07 20:35:21 +0300

    Done

--- myscript.js
+++ myscript.js
@@ -4,11 +4,50 @@ let board = [
     ['20', '21', '22']
 ]
 
+let W = "O";
+
+function press(x,y) {
+
+    console.log(x,y)
 
 
-function press(cellNO) {
+    if(W == `X`){
+        W = `O`;
+    }
+    else{
+        W = `X`
+    }
 
-    // Do stuff
-    console.log(cellNO)
-    
+    board[x][y] = W;
+    let cell = document.getElementById(`${x}${y}`);
+    cell.innerText = W;
+
+    if (board[0][2] == W && board[0][1] == W && board[0][0] == W){
+        document.getElementById("status").innerText = `كفوووو ${W}`;
+    }
+    else if(board[0][0] == W && board[1][1] == W && board[2][2] == W){
+        document.getElementById("status").innerText = `كفوووو ${W}`;
+    }
+    else if(board[0][0] == W && board[1][0] == W && board[2][0] == W){
+        document.getElementById("status").innerText = `كفوووو ${W}`;
+    }
+    else if(board[0][1] == W && board[1][1] == W && board[2][1] == W){
+        document.getElementById("status").innerText = `كفوووو ${W}`;
+    }
+    else if(board[0][2] == W && board[1][2] == W && board[2][2] == W){
+        document.getElementById("status").innerText = `كفوووو ${W}`;
+    }
+    else if(board[1][0] == W && board[1][1] == W && board[1][2] == W){
+        document.getElementById("status").innerText = `كفوووو ${W}`;
+    }
+    else if(board[2][0] == W && board[2][1] == W && board[2][2] == W){
+        document.getElementById("status").innerText = `كفوووو ${W}`;
+    }
+        else if(board[0][2] == W && board[1][1] == W && board[2][0] == W){
+        document.getElementById("status").innerText = `كفوووو ${W}`;
+    }
 }
+
+
+
+    
